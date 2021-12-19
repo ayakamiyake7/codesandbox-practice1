dcfc39f5e0147c1745f958ffc95a309d8ed7576a
ayakamiyake7 <88525743+ayakamiyake7@users.noreply.github.com>
2021-12-19 19:32:48 +0000

    Initial commit

---
+++ src/ChildArea.jsx
@@ -0,0 +1,28 @@
+import { memo } from "react";
+
+const style = {
+  width: "100%",
+  height: "200px",
+  backgroundColor: "khaki"
+};
+
+export const ChildArea = memo((props) => {
+  const { open, onClickClose } = props;
+  console.log("rendering!");
+
+  const data = [...Array(1000).keys()];
+  data.forEach(() => {
+    console.log("...");
+  });
+
+  return (
+    <>
+      {open ? (
+        <div style={style}>
+          <p>Component</p>
+          <button onClick={onClickClose}>Close</button>
+        </div>
+      ) : null}
+    </>
+  );
+});
